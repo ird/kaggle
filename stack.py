86fd7c5109487dce1a26e49d3ceb5af2bae44809
ird <31051194+ird@users.noreply.github.com>
2017-10-07 22:33:40 +0100

    benchmark(), reverse_rows() and reverse_cols()

--- stack.py
+++ stack.py
@@ -1,4 +1,43 @@
 import numpy as np
+from timeit import Timer
+
+
+def pure_python():
+    X = range(1000)
+    Y = range(1000)
+    Z = []
+    for i in range(len(X)):
+        Z.append(X[i] + Y[i])
+    return len(Z)
+
+
+def np_version():
+    X = np.arange(1000)
+    Y = np.arange(1000)
+    Z = X + Y
+    return len(Z)
+
+
+def benchmark(fname1, fname2):
+    timer1 = Timer(fname1 + "()", "from __main__ import " + fname1)
+    timer2 = Timer(fname2 + "()", "from __main__ import " + fname2)
+    return timer1.timeit(1) / timer2.timeit(1)
+
+
+def reverse_rows(m):
+    M = m.copy()
+    y, _ = np.shape(M)
+    for i in range(y):
+        M[i] = M[i][::-1]
+    return M
+
+
+def reverse_cols(m):
+    M = m.copy()
+    _, x = np.shape(M)
+    for i in range(x):
+        M[:, i] = M[:, i][::-1]
+    return M
 
 
 def main():
@@ -8,6 +47,14 @@ def main():
     d = np.stack((b, a), axis=-1)
     print(c)
     print(d)
+    # np arrays vs python arrays
+    print(benchmark("pure_python", "np_version"))
+    # exercise 5&6 - numpy (python-course.eu)
+    # reverse the rows and cols of a multidimensional array
+    m = np.arange(28).reshape(4, 7)
+    print(m)
+    print(reverse_rows(m))
+    print(reverse_cols(m))
     return
 
 
