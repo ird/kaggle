bf61123cf1ad84e41d5e4ca17f4c36f0fb43a6e8
ird <31051194+ird@users.noreply.github.com>
2017-10-29 16:55:43 +0000

    sol2: DecisionTreeClassifier

---
+++ titanic/sol2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree
+
+train = pd.read_csv('train.csv')
+train['Sex'][train['Sex'] == 'male'] = 0
+train['Sex'][train['Sex'] == 'female'] = 1
+
+train['Age'] = train['Age'].fillna(train.Age.median())
+train['Pclass'] = train['Pclass'].fillna(train.Pclass.median())
+train['Fare'] = train['Fare'].fillna(train.Fare.median())
+
+target = train['Survived'].values
+features = train[['Pclass', 'Sex', 'Age', 'Fare']].values
+
+tr = tree.DecisionTreeClassifier()
+tr = tr.fit(features, target)
+print(tr.feature_importances_)
+
+test = pd.read_csv('test.csv')
+test['Sex'][test['Sex'] == 'male'] = 0
+test['Sex'][test['Sex'] == 'female'] = 1
+
+test['Age'] = test['Age'].fillna(test.Age.median())
+test['Pclass'] = test['Pclass'].fillna(test.Pclass.median())
+test['Fare'] = test['Fare'].fillna(test.Fare.median())
+
+test_features = test[['Pclass', 'Sex', 'Age', 'Fare']]
+test['Survived'] = tr.predict(test_features)
+test.to_csv('sol2.csv', index=False,
+            columns=['PassengerId', 'Survived'])
+# PassengerId = np.array(test['PassengerId']).astype(int)
+# sol2 = pd.DataFrame(pred, PassengerId, columns=['Survived'])
+# print(sol2)
