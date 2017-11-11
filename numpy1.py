a236934eaa2f2b280089138be0fae8e6ab0bd235
ird <31051194+ird@users.noreply.github.com>
2017-11-11 10:02:42 +0000

    numpy1.py: numpy basics

--- numpy1.py
+++ numpy1.py
@@ -4,16 +4,26 @@ import numpy as np
 def main():
     # use np.zeros() to create a np array full of zeros
     A = np.zeros(5, "int64")
-    print(A)
     print(inspect(A))
     B = np.zeros((4, 5), "complex")
-    print(B)
     print(inspect(B))
+    # or pass a sequence to np.array()
+    C = np.array([1.5, 3, 3, 6])
+    D = np.array((1.5, 3, 3, 6))
+    print(inspect(C))
+    print(inspect(D))
 
 
 def inspect(n):
-    r = "Type of <" + hex(id(n)) + "> " + str(type(n)) + "\n"
-    r += "Data type of <" + hex(id(n)) + "> " + str(n.dtype) + "\n"
+    # r = "Contents of <" + hex(id(n)) + "> " + str(n) + " \n"
+    # r += "Type of <" + hex(id(n)) + "> " + str(type(n)) + "\n"
+    # r += "Data type of <" + hex(id(n)) + "> " + str(n.dtype) + "\n"
+    r = ""
+    characteristics = {'Contents of': str(n),
+                       'Type of': str(type(n)),
+                       'Data type of': str(n.dtype)}
+    for item in characteristics:
+        r += "{} {}: {}\n".format(item, hex(id(n)), characteristics[item])
     return r
 
 
