e2a4580eeb6b929407d0ffa97ad009bfdf2b2626
ird <31051194+ird@users.noreply.github.com>
2017-11-11 09:45:36 +0000

    numpy1.py: numpy basics

--- numpy1.py
+++ numpy1.py
@@ -5,8 +5,16 @@ def main():
     # use np.zeros() to create a np array full of zeros
     A = np.zeros(5, "int64")
     print(A)
-    print("Type of A: ", type(A))
-    print("Data type of A: ", A.dtype)
+    print(inspect(A))
+    B = np.zeros((4, 5), "complex")
+    print(B)
+    print(inspect(B))
+
+
+def inspect(n):
+    r = "Type of <" + hex(id(n)) + "> " + str(type(n)) + "\n"
+    r += "Data type of <" + hex(id(n)) + "> " + str(n.dtype) + "\n"
+    return r
 
 
 if __name__ == "__main__":
